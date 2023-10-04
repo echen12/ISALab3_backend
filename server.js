785e966fde008aa6f577f3405fa31e2b79e1d199
echen12 <chen.edmond13@gmail.com>
2023-10-04 10:55:55 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,77 @@
+const http = require('http');
+const port = 3000; // Change to the desired port
+const url = require('url');
+const querystring = require('querystring');
+
+
+
+const dictionary = [];
+
+let numRequests = 0
+
+function DictionaryEntry(word, def) {
+    this.word = word;
+    this.def = def;
+}
+
+
+const server = http.createServer((req, res) => {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+
+    if (req.method === 'GET' && url.pathname === "/api/definitions/") {
+        
+
+        let urlQueryParams = url.searchParams.get("word")
+
+        
+        console.log(dictionary.find(d => d.word === urlQueryParams))
+        if (dictionary.find(d => d.word === urlQueryParams) !== undefined) {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({ message: dictionary.find(d => d.word === urlQueryParams), numberOfReq: ++numRequests}));
+        } else if (urlQueryParams === null || dictionary.find(d => d.word === urlQueryParams) === undefined) {
+            res.statusCode = 404;
+            res.end('Not Found');
+        }
+
+    } else if (req.method === 'POST' && url.pathname === "/api/definitions") {
+        
+        let data = '';
+
+        req.on('data', (chunk) => {
+            console.log(chunk.toString())
+            data += chunk.toString();
+        });
+
+        req.on('end', () => {
+            data = data.replace('?', '');
+            const queryParams = querystring.parse(data)
+
+            if (queryParams.word === undefined || queryParams.definition === undefined || (dictionary.find(d => d.word === queryParams.word) !== undefined && dictionary.find(d => d.def === queryParams.definition) !== undefined)) {
+                res.statusCode = 400;
+                res.end('Invalid request body, both the word and definition must be provided.');
+            }
+            else if (queryParams.word && queryParams.definition) {
+                const newEntry = new DictionaryEntry(queryParams.word, queryParams.definition)
+
+                if (!dictionary.some(el => el.word === queryParams.word)) {
+                    dictionary.push(newEntry)
+                }else {
+                    let dictIndex = dictionary.findIndex(e => e.word === queryParams.word);
+                    dictionary[dictIndex].def = queryParams.definition
+                }
+
+                res.setHeader('Content-Type', 'application/json');
+                res.end(JSON.stringify({ message: 'POST request received', data: JSON.stringify(dictionary), numberOfReq: ++numRequests }));
+            }
+        });
+    } else {
+        
+        res.statusCode = 404;
+        res.end('Not Found');
+    }
+});
+
+
+server.listen(port, () => {process.env.PORT ||
+    console.log(`Server is running on port ${port}`);
+});
